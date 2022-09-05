5af2e9523a531b68087a823786ccda25f371745d
Juan Torres <juantorresvelilla@gmail.com>
2022-09-05 14:19:39 -0500

    solución a error eliminar todo

--- src/routes/metodos.js
+++ src/routes/metodos.js
@@ -15,7 +15,18 @@ router.delete('/:Sku', (req, res) => {
         productos.splice(i, 1);
     }
    });
-   res.send(productos);
+   res.send('deleted');
+});
+router.post('/', (req, res) => {
+    const { Nombre, Precio,URL, Marca, Iva, Inventario}  = req.body;
+    if(Nombre && Precio && URL && Marca && Iva && Inventario){
+        const Sku = productos.length + 1;
+        const newProduct = {...req.body, Sku};
+        productos.push(newProduct);
+        res.send('se insertó el producto satisfactoriamente.');
+    }else{
+        res.send('No se inserto el Producto satisfactoriamente');
+    }
 });
 
 module.exports = router;
