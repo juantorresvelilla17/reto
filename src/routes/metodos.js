98ba500dd9e9aa363697622ffc4c89a5293859ec
Juan Torres <juantorresvelilla@gmail.com>
2022-09-05 02:50:20 -0500

    endpoint insertar producto realizado

--- src/routes/metodos.js
+++ src/routes/metodos.js
@@ -8,5 +8,16 @@ router.get('/', (req, res) => {
     res.json(productos);
 })
 
+router.post('/', (req, res) => {
+    const { Nombre, Precio,URL, Marca, Iva, Inventario}  = req.body;
+    if(Nombre && Precio && URL && Marca && Iva && Inventario){
+        const Sku = productos.length + 1;
+        const newProduct = {...req.body, Sku};
+        productos.push(newProduct);
+        res.send('se insertó el producto satisfactoriamente.');
+    }else{
+        res.send('No se inserto el Producto satisfactoriamente');
+    }
+});
 
 module.exports = router;
