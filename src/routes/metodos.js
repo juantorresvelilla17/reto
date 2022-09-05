8fa33953d291a53b9ddb1ac392b9fc98a1bf4530
Juan Torres <juantorresvelilla@gmail.com>
2022-09-05 03:21:16 -0500

    endpoint eliminar producto finalizado

--- src/routes/metodos.js
+++ src/routes/metodos.js
@@ -8,5 +8,14 @@ router.get('/', (req, res) => {
     res.json(productos);
 })
 
+router.delete('/:Sku', (req, res) => {
+    const {Sku} = req.params;
+   _.each(productos, (producto, i) => {
+    if(producto.Sku == Sku){
+        productos.splice(i, 1);
+    }
+   });
+   res.send(productos);
+});
 
 module.exports = router;
